a55d03ff6a497670cc419052f77b408e60e6a0d4
yograjm <yograj.m@talentsprint.com>
2025-05-09 14:11:54 +0000

    python file added to append data

--- README.md
+++ README.md
@@ -1,1 +1,92 @@
 # airflow-repo
+
+
+## Steps:
+
+- Start a Postgres docker container in an Amazon EC2 instance
+  
+  Launch and EC2 instance and install Docker in it
+  ```yml
+  sudo apt-get update
+  sudo apt-get upgrade -y
+  sudo apt install docker.io -y
+  sudo systemctl start docker
+  sudo systemctl enable docker
+  sudo docker --version
+  ```
+
+  To start a DB container:
+  `sudo docker run -it -d -p 5432:5432 -e POSTGRES_PASSWORD=mypassword --name=postgrescont postgres:latest`
+  
+- Create a Postgres database - `storedb`
+
+  To enter into container:
+  `sudo docker exec -it postgrescont psql -U postgres`
+
+  To list all databases: 
+  `SELECT datname FROM pg_database WHERE datistemplate = false;`
+
+  To create a new DB
+  `CREATE DATABASE storedb;`
+
+  To switch to a particular DB:
+  `\c storedb;`
+
+- Create a table - `loans_data`
+
+  To see tables inside a DB:
+  `\dt`
+
+  To create table:
+    ```yml
+    CREATE TABLE loans_data (
+        person_age INT,
+        person_income INT,
+        person_home_ownership VARCHAR(20),
+        person_emp_length FLOAT,
+        loan_intent VARCHAR(20),
+        loan_grade CHAR(1),
+        loan_amnt INT,
+        loan_int_rate FLOAT,
+        loan_status INT,
+        loan_percent_income FLOAT,
+        cb_person_default_on_file CHAR(1),
+        cb_person_cred_hist_length INT
+    );
+    ```
+
+    To check table rows: (Press `q` if the output is being displayed in a pager)
+
+    `SELECT * FROM loans_data LIMIT 10;`
+
+    `SELECT * FROM loans_data ORDER BY person_age DESC LIMIT 10;`
+
+    `SELECT COUNT(*) FROM loans_data;`
+
+    Use `\d` to quit.
+
+- On Codespace, create a Python script, `process_data.py`, to add data to `storedb` from the csv file
+  - Load date from the csv file
+  - Add data to db
+
+- Airflow to run the Python script `process_data.py` periodically
+  - Ref (https://airflow.apache.org/docs/apache-airflow/stable/howto/docker-compose/index.html)
+
+    `curl -LfO 'https://airflow.apache.org/docs/apache-airflow/2.10.5/docker-compose.yaml'`
+
+    `mkdir -p ./dags ./logs ./plugins ./config`
+
+    Add `.py` files to `./dags` folder
+
+    `echo -e "AIRFLOW_UID=$(id -u)" > .env`
+
+    `docker compose up airflow-init`
+
+    `docker compose up`
+
+  - UI running
+  - DAG creating
+  - Pipeline execution success - data appending to the DB
+  - Stop Airflow: `docker compose down --volumes --rmi all`
+
+
---
+++ processing_data.py
@@ -0,0 +1,101 @@
+
+import pandas as pd
+import psycopg2
+from psycopg2 import sql
+
+# Database connection parameters
+db_params = {
+    'dbname': 'storedb',
+    'user': 'postgres',
+    'password': 'mypassword',
+    'host': '127.0.0.1',  # EC2 public IP # or your database host
+    'port': '5432'  #'5432'        # default PostgreSQL port
+}
+
+
+def append_data_to_db():
+
+    # Read the CSV file
+    csv_file_path = 'https://raw.githubusercontent.com/yograjm/loan-demo/refs/heads/main/loan_defaulter_pred/dataset/credit_risk_dataset.csv'
+    data = pd.read_csv(csv_file_path)
+
+    # Connect to the PostgreSQL database
+    try:
+        conn = psycopg2.connect(**db_params)
+        cursor = conn.cursor()
+
+        # Read existing data from the table
+        query = "SELECT * FROM loans_data;"  # SQL query to select all data from the table
+        cursor.execute(query)
+
+        # Fetch all results
+        rows = cursor.fetchall()
+
+        # Get column names from the cursor
+        column_names = [desc[0] for desc in cursor.description]
+
+        # Create a DataFrame from the fetched data
+        df = pd.DataFrame(rows, columns=column_names)
+        print(f"Existing rows in db: {len(df)}")
+
+        # Display the DataFrame
+        #print(df)
+
+        # Next Row to add to db
+        curr_rows = len(df)
+        if curr_rows >= len(data):
+            curr_rows = curr_rows - len(data)
+        
+
+        # Insert data into the passengers table
+        row_to_add = data.iloc[[curr_rows], :]     # add row one-by-one
+        #row_to_add = data.iloc[curr_rows:curr_rows+4]        # add 4 rows at once
+        #row_to_add = data.iloc[curr_rows:]        # add all rows at once
+        # print(row_to_add)
+
+        count = 0
+
+        for index, row in row_to_add.iterrows():
+            cursor.execute(
+                sql.SQL("""
+                    INSERT INTO loans_data (
+                        person_age, person_income, person_home_ownership, person_emp_length,
+                        loan_intent, loan_grade, loan_amnt, loan_int_rate, loan_status,
+                        loan_percent_income, cb_person_default_on_file, cb_person_cred_hist_length
+                    ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                """),
+                (
+                    row['person_age'],
+                    row['person_income'],
+                    row['person_home_ownership'],
+                    row['person_emp_length'],
+                    row['loan_intent'],
+                    row['loan_grade'],
+                    row['loan_amnt'],
+                    row['loan_int_rate'],
+                    row['loan_status'],
+                    row['loan_percent_income'],
+                    row['cb_person_default_on_file'],
+                    row['cb_person_cred_hist_length']
+                )
+            )
+            count += 1
+
+        # Commit the transaction
+        conn.commit()
+        print(f"Data inserted successfully.\nCount after adding new rows: {len(df) + count}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    finally:
+        # Close the cursor and connection
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+
+
+if __name__ == "__main__":
+    append_data_to_db()
+    
